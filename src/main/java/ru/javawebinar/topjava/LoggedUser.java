e5da0546845185e8371d735406557eff7f8e65a9
Mellifaro <housse1109@gmail.com>
2016-09-06 16:12:45 +0300

    HW2(06/09/2016)

---
+++ src/main/java/ru/javawebinar/topjava/LoggedUser.java
@@ -0,0 +1,23 @@
+package ru.javawebinar.topjava;
+
+import ru.javawebinar.topjava.util.UserMealsUtil;
+
+/**
+ * GKislin
+ * 06.03.2015.
+ */
+public class LoggedUser {
+    public static int id = 1;
+
+    public static int id() {
+        return id;
+    }
+
+    public static void setId(int id) {
+        LoggedUser.id = id;
+    }
+
+    public static int getCaloriesPerDay() {
+        return UserMealsUtil.DEFAULT_CALORIES_PER_DAY;
+    }
+}
